7bf73dbf0b12ad615cf6b04e5ecc401cdccab76d
NutchapolSal <Nutchapol.sal@gmail.com>
2023-07-20 15:06:47 +0700

    add average stat

--- lab3q4_65251.c
+++ lab3q4_65251.c
@@ -2,11 +2,18 @@
 int main() {
 	int a = 0;
 	int b = 0;
+	int count = -1;
 	do {
 		b += a;
+		count++;
 		printf("enter a number : ");
 		scanf("%d",&a);
 	} while (0 < a);
-	printf("summation = %d\n", b);
+	if (count == 0) {
+		printf("you entered no numbers! :(\n");
+	} else {
+		printf("summation = %d\n", b);
+		printf("average = %f\n", b / (double) count);
+	}
 	return 0;
 }
